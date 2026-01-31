d9222fd276285fdb7b62c6f0b7040e64646efcdc
Thomas Ernst <thomas.ernst@ebenefuenf.de>
2026-01-31 21:36:21 +0100

    Fix test namespace blocks

--- tests/SessionNameIdFailureTest.php
+++ tests/SessionNameIdFailureTest.php
@@ -14,61 +14,61 @@ namespace Duon\Session {
 	}
 }
 
-namespace Duon\Session\Tests;
+namespace Duon\Session\Tests {
+	use Duon\Session\RuntimeException;
+	use Duon\Session\Session;
 
-use Duon\Session\RuntimeException;
-use Duon\Session\Session;
-
-final class SessionNameIdFailureTest extends TestCase
-{
-	private static bool $forceNameFalse = false;
-	private static bool $forceIdFalse = false;
-
-	public static function sessionNameResult(): string|false
+	final class SessionNameIdFailureTest extends TestCase
 	{
-		if (self::$forceNameFalse) {
-			return false;
+		private static bool $forceNameFalse = false;
+		private static bool $forceIdFalse = false;
+
+		public static function sessionNameResult(): string|false
+		{
+			if (self::$forceNameFalse) {
+				return false;
+			}
+
+			return \session_name();
 		}
 
-		return \session_name();
-	}
+		public static function sessionIdResult(): string|false
+		{
+			if (self::$forceIdFalse) {
+				return false;
+			}
 
-	public static function sessionIdResult(): string|false
-	{
-		if (self::$forceIdFalse) {
-			return false;
+			return \session_id();
 		}
 
-		return \session_id();
-	}
+		protected function tearDown(): void
+		{
+			self::$forceNameFalse = false;
+			self::$forceIdFalse = false;
 
-	protected function tearDown(): void
-	{
-		self::$forceNameFalse = false;
-		self::$forceIdFalse = false;
+			parent::tearDown();
+		}
 
-		parent::tearDown();
-	}
+		public function testNameThrowsWhenUnavailable(): void
+		{
+			self::$forceNameFalse = true;
+			$session = new Session();
 
-	public function testNameThrowsWhenUnavailable(): void
-	{
-		self::$forceNameFalse = true;
-		$session = new Session();
+			$this->expectException(RuntimeException::class);
+			$this->expectExceptionMessage('Session name not available');
 
-		$this->expectException(RuntimeException::class);
-		$this->expectExceptionMessage('Session name not available');
+			$session->name();
+		}
 
-		$session->name();
-	}
+		public function testIdThrowsWhenUnavailable(): void
+		{
+			self::$forceIdFalse = true;
+			$session = new Session();
 
-	public function testIdThrowsWhenUnavailable(): void
-	{
-		self::$forceIdFalse = true;
-		$session = new Session();
+			$this->expectException(RuntimeException::class);
+			$this->expectExceptionMessage('Session id not available');
 
-		$this->expectException(RuntimeException::class);
-		$this->expectExceptionMessage('Session id not available');
-
-		$session->id();
+			$session->id();
+		}
 	}
 }
